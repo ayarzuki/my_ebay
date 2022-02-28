0dde15b6dc10777f083bba30efb0c67678404284
ayarzuki <aulya.yarzuki@gmail.com>
2022-02-28 22:05:11 +0700

    create seeder, create search parser template

---
+++ seeder/seeder.rb
@@ -0,0 +1,14 @@
+pages << { 
+    url: 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=switch&_sacat=0',
+    page_type: 'search',
+    # fetch_type: 'fullbrowser',
+    # method: 'POST',
+    # body: JSON.generate({'abc' => 111}),
+    # headers: [
+    #     'content-type' => 'application-json',
+    #     'cookie' => 'dsfdsfa=alksdfklasd'
+    # ]
+    # vars: { 
+    #     '123' => 'abc'
+    # }
+}
